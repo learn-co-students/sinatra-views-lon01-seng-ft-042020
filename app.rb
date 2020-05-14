24140db2ab41e228ae4fe753c4108664ae10fda8
github username <github email address>
2020-05-14 03:14:06 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	#Returns a 200 status code
+	#Loads the index.erb file
 	get '/' do
-		
+	 erb :index
 	end
+
+	#Returns a 200 status code
+	#Loads info.erb in the view
+	get "/info" do
+		erb :info
+	  end
 end
